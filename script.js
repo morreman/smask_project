50128d64937607786af231e725aa1c2eb2017a14
Anton <anton.h.nilsson@gmail.com>
2016-12-29 20:48:54 +0100

    small changes, in progress

--- script.js
+++ script.js
@@ -1,50 +1,33 @@
 var songs= [];
 
-$(".btn-warning").click(function(){
-  apa = $(".btn-warning").text();
-  localStorage.setItem("lastname", apa);
-});
-$(".btn-danger").click(function(){
-  apa = $(".btn-danger").text();
-  localStorage.setItem("lastname", apa);
-});
-$(".btn-success").click(function(){
-  apa = $(".btn-success").text();
-  localStorage.setItem("lastname", apa);
-});
-$(".btn-info").click(function(){
-  apa = $(".btn-info").text();
-  localStorage.setItem("lastname", apa);
-});
-$(".btn-primary").click(function(){
-  apa = $(".btn-primary").text();
-  localStorage.setItem("lastname", apa);
-});
-$(".btn-default").click(function(){
-  apa = $(".btn-default").text();
-  localStorage.setItem("lastname", apa);
-});
-
 $("#submit").click(function(){
   addSongToJson();
+  songlist = JSON.parse(localStorage.getItem("songs"));
+  updateList(songlist);
 });
 
 function addSongToJson(){
   if(JSON.parse(localStorage.getItem("songs") == null)){
     songn = $("#songname").val();
     artist = $("#artistname").val();
-    song = {"songname" : songn, "artist" : artist};
+    song = {"songname" : songn, "artist" : artist, "points": 0};
     songs.push(song);
     localStorage.setItem("songs", JSON.stringify(songs));
   }else{
     songs = JSON.parse(localStorage.getItem("songs"));
     songn = $("#songname").val();
     artist = $("#artistname").val();
-    song = {"songname" : songn, "artist" : artist};
+    song = {"songname" : songn, "artist" : artist, "points": 0};
     console.log(song);
     songs.push(song);
     localStorage.setItem("songs", JSON.stringify(songs));
     $("#songname").val("");
     $("#artistname").val("");
     }
+}
+function updateList(songlist){
+  for(var i = 0; i < songlist.length; i++){
+    $("#songlist").append("<div id='songlistsong'" + i + "><div id ='text' class='row'><div class='col-md-10'><h1>"
+    + songlist[i].songname + "</h1></div><div id = 'buttons' class='row'><div class='col-md-10'><button type='button' class='btn btn-default'>Default</button></div></div></div>");
+  }
 }
--- scriptsecond.js
+++ scriptsecond.js
@@ -3,10 +3,9 @@ $(document).ready(
             setInterval(function() {
                     $(".wrapper").empty();
                     var songs = JSON.parse(localStorage.getItem("songs"));
-                    console.log(songs[0].songname);
                     for(var i = 0; songs.length; i++){
-                      console.log(songs[i].songname);
-                      $(".wrapper").append("<div id=songdiv><div id='song"+ i +"'><h1>" + songs[i].songname +"</h1></div><div id='artist" + i + "'><h3>" + songs[i].artist + "</h3></div></div>");
+                      $(".wrapper").append("<div class='row'><div id=songdiv class='col-md-8'><div id='song"+ i +"'><h1>" + songs[i].songname +"</h1></div><div id='artist"
+                       + i + "'><h3>" + songs[i].artist + "</h3></div></div><div class='col-md-4' id='pointdiv'><h3>" + songs[i].points + "</h3></div></div>");
                     }
                   }, 1000);
             });
