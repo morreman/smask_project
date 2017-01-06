659afccb768923010a5bab7272add508c3f0e8a2
Marten <persson_dunk@msn.com>
2017-01-06 10:47:03 +0100

    prevent errors

--- script.js
+++ script.js
@@ -1,33 +1,45 @@
-var songs= [];
+var songs = [];
 
-$("#submit").click(function(){
-  addSongToJson();
-  songlist = JSON.parse(localStorage.getItem("songs"));
-  updateList(songlist);
+$("#submit").click(function(e) {
+    addSongToJson();
+    songlist = JSON.parse(localStorage.getItem("songs"));
+    updateList(songlist);
+    e.preventDefault();
 });
 
-function addSongToJson(){
-  if(JSON.parse(localStorage.getItem("songs") == null)){
-    songn = $("#songname").val();
-    artist = $("#artistname").val();
-    song = {"songname" : songn, "artist" : artist, "points": 0};
-    songs.push(song);
-    localStorage.setItem("songs", JSON.stringify(songs));
-  }else{
-    songs = JSON.parse(localStorage.getItem("songs"));
-    songn = $("#songname").val();
-    artist = $("#artistname").val();
-    song = {"songname" : songn, "artist" : artist, "points": 0};
-    console.log(song);
-    songs.push(song);
-    localStorage.setItem("songs", JSON.stringify(songs));
-    $("#songname").val("");
-    $("#artistname").val("");
+function addSongToJson() {
+    if (JSON.parse(localStorage.getItem("songs") == null)) {
+        songn = $("#songname").val();
+        artist = $("#artistname").val();
+        points = $('.radio_select:checked').val();
+        song = {
+            "songname": songn,
+            "artist": artist,
+            "points": points
+        };
+        songs.push(song);
+        localStorage.setItem("songs", JSON.stringify(songs));
+    } else {
+        songs = JSON.parse(localStorage.getItem("songs"));
+        songn = $("#songname").val();
+        artist = $("#artistname").val();
+        points = $('.radio_select:checked').val();
+        song = {
+            "songname": songn,
+            "artist": artist,
+            "points": points
+        };
+        console.log(song);
+        songs.push(song);
+        localStorage.setItem("songs", JSON.stringify(songs));
+        $("#songname").val("");
+        $("#artistname").val("");
     }
 }
-function updateList(songlist){
-  for(var i = 0; i < songlist.length; i++){
-    $("#songlist").append("<div id='songlistsong'" + i + "><div id ='text' class='row'><div class='col-md-10'><h1>"
-    + songlist[i].songname + "</h1></div><div id = 'buttons' class='row'><div class='col-md-10'><button type='button' class='btn btn-default'>Default</button></div></div></div>");
-  }
+
+function updateList(songlist) {
+    for (var i = 0; i < songlist.length; i++) {
+        $("#songlist").append("<div id='songlistsong'" + i + "><div id ='text' class='row'><div class='col-md-10'><h1>" +
+            songlist[i].songname + "</h1></div><div id = 'buttons' class='row'><div class='col-md-10'><button type='button' class='btn btn-default'>Default</button></div></div></div>");
+    }
 }
--- scriptsecond.js
+++ scriptsecond.js
@@ -1,11 +1,14 @@
 $(document).ready(
-        function() {
-            setInterval(function() {
-                    $(".wrapper").empty();
-                    var songs = JSON.parse(localStorage.getItem("songs"));
-                    for(var i = 0; songs.length; i++){
-                      $(".wrapper").append("<div class='row'><div id=songdiv class='col-md-8'><div id='song"+ i +"'><h1>" + songs[i].songname +"</h1></div><div id='artist"
-                       + i + "'><h3>" + songs[i].artist + "</h3></div></div><div class='col-md-4' id='pointdiv'><h3>" + songs[i].points + "</h3></div></div>");
-                    }
-                  }, 1000);
-            });
+    function() {
+      var canvas = $('#canvas');
+        setInterval(function() {
+            $(".wrapper").empty();
+            var songs = JSON.parse(localStorage.getItem("songs"));
+            if (songs != null) {
+                for (var i = 0; i < songs.length; i++) {
+                    $(".wrapper").append("<div class='row col-sm-12'><div id=songdiv><div class='col-sm-4' id='song" + i + "'><h1>" + songs[i].songname + "</h1></div><div class='col-sm-4' id='artist" +
+                        i + "'><h3>" + songs[i].artist + "</h3></div><div class='col-sm-2' id='pointdiv'><h3><b>" + songs[i].points + "</b></h3></div></div></div>");
+                }
+            }
+        }, 1000);
+    });
