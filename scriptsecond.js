c8815718c3f8dfee1fb2cf76c68b5dcc6d7d190b
Anton <anton.h.nilsson@gmail.com>
2017-01-13 14:06:09 +0100

    display changes

--- scriptsecond.js
+++ scriptsecond.js
@@ -5,8 +5,9 @@ $(document).ready(
             var songs = JSON.parse(localStorage.getItem("songs"));
             if (songs != null) {
                 for (var i = 0; i < songs.length; i++) {
-                    $(".wrapper").append("<div class='row col-sm-12'><div id=songdiv><div class='col-sm-4' id='song" + i + "'><h1>" + songs[i].songname + "</h1></div><div class='col-sm-4' id='artist" +
-                        i + "'><h3>" + songs[i].artist + "</h3></div><div class='col-sm-2' id='pointdiv'><h3><b>" + songs[i].currentpoints + "</b></h3></div><div class='col-sm-2' id='pointdiv'><h3><b>" + songs[i].points + "</b></h3></div></div></div>");
+                    $(".wrapper").append("<div id='apa' class='row'><div id=songdiv><div class='col-sm-6' id='song" + i + "'><h1>" + songs[i].songname + "</h1><div class='row col-sm-12' id='artist" + i + "'><h3>" + songs[i].artist +
+                    "</h3></div></div><div class='col-sm-2' id='pointdiv'><h1 size='100px'><b>" + songs[i].currentpoints + "</b></h1></div><div class='col-sm-2' id='pointdiv'><h1><b>" + songs[i].points +
+                    "</b></h1></div><div class='row col-sm-12'></div></div></div><hr>");
                 }
             }
         }, 1000);
